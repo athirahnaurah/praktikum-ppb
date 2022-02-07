804cdd5839ea45d18130f06cf6bf1b6da86420c2
athirahnaurah <athirah.naurah.tif20@polban.ac.id>
2022-02-07 12:37:06 +0700

    week 2

---
+++ HomeFan/app/src/main/java/com/example/homefan/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.homefan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity{
+
+    private Button btn;
+    ToggleButton toggleButton;
+    ImageView imageView;
+    ObjectAnimator rotateAnimator;
+    Switch switchButton;
+    SeekBar seekBar;
+    final int SPEED[] = {0, 5000, 3000, 1000};
+    GradientDrawable gd = new GradientDrawable();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rotateAnimator = ObjectAnimator.ofFloat(imageView, "rotation", 0, 360);
+        rotateAnimator.setDuration(1000);
+        rotateAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        rotateAnimator.setInterpolator(new LinearInterpolator());
+    }
+
+    @Override
+    public void onClick(View v){
+        CharSequence btnText = btn.getText();
+        if(btnText == "OFF"){
+            btn.setText("ON");
+        }
+        else{
+            btn.setText("OFF");
+        }
+    }
+}
